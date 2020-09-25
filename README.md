b586cc0b51850aa907733751e7e4c1c701b2d552
ymwho <parametertree@gmail.com>
2020-09-25 15:22:00 +0900

    red

--- README.md
+++
@@ -1,9 +0,0 @@
-# SubTest Project !
-
-- [] the Glam ( Main)
-- [] 저희 사이트는
-- [] 저희 제품은
-- [] 고객님센터
-- [] Main Carousel
-
-- [] Menu List
---
+++ build/precache-manifest.70caebd75673eaec65ba0df36e05cd59.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393f628542286b070d2298a98c310997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e3dc73a7b2122d9669",
+    "url": "/static/js/2.cf6d73a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf6d73a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0697d35c1ae09d657fe6",
+    "url": "/static/js/main.4c5ea169.chunk.js"
+  },
+  {
+    "revision": "de9f9fe70ba78ea5c14d",
+    "url": "/static/js/runtime-main.d2755cd4.js"
+  },
+  {
+    "revision": "560bf3391868baf5999cece4b864472e",
+    "url": "/static/media/0.560bf339.jpg"
+  },
+  {
+    "revision": "1f1aa29d1448cff4bc1b308be286f5df",
+    "url": "/static/media/1.1f1aa29d.jpg"
+  },
+  {
+    "revision": "8318b6267ec46494e369e4d0c1f3046c",
+    "url": "/static/media/2.8318b626.jpg"
+  },
+  {
+    "revision": "afa2276644913f30217d353e712c2250",
+    "url": "/static/media/3.afa22766.jpg"
+  },
+  {
+    "revision": "e7872bcc70aebf0422c5325e2ad1433e",
+    "url": "/static/media/4.e7872bcc.jpg"
+  },
+  {
+    "revision": "0a95648df9a7cd33de3c5ff8fe93ac32",
+    "url": "/static/media/5.0a95648d.jpg"
+  },
+  {
+    "revision": "dd960c1f29cb800324a0c53702bdb5c7",
+    "url": "/static/media/6.dd960c1f.jpg"
+  },
+  {
+    "revision": "e0b1d34973aa03eca51ef796afc960ad",
+    "url": "/static/media/7.e0b1d349.jpg"
+  },
+  {
+    "revision": "bd1a829c34cab196b49e0c0f657c1c8c",
+    "url": "/static/media/8.bd1a829c.jpg"
+  },
+  {
+    "revision": "a18ba9fb834b058f132cc90885b4647a",
+    "url": "/static/media/Site.a18ba9fb.jpg"
+  },
+  {
+    "revision": "15cf8cdca78d2ed3b935571ff1abe0a6",
+    "url": "/static/media/Site2.15cf8cdc.jpg"
+  },
+  {
+    "revision": "27eb6c77314b8a5a3b89b4d57fea81f0",
+    "url": "/static/media/Site3.27eb6c77.jpg"
+  },
+  {
+    "revision": "d0721b968d597036b46cebf7a4d6b03d",
+    "url": "/static/media/Site4.d0721b96.jpg"
+  },
+  {
+    "revision": "44120764beb4e686d2f8ae5d660c9ccb",
+    "url": "/static/media/Site5.44120764.jpg"
+  },
+  {
+    "revision": "afa2276644913f30217d353e712c2250",
+    "url": "/static/media/Site6.afa22766.jpg"
+  },
+  {
+    "revision": "e7872bcc70aebf0422c5325e2ad1433e",
+    "url": "/static/media/Site7.e7872bcc.jpg"
+  },
+  {
+    "revision": "1f1aa29d1448cff4bc1b308be286f5df",
+    "url": "/static/media/Site8.1f1aa29d.jpg"
+  },
+  {
+    "revision": "8318b6267ec46494e369e4d0c1f3046c",
+    "url": "/static/media/Site9.8318b626.jpg"
+  },
+  {
+    "revision": "8328a6c8c2edf6ea85ceed28d45dc2c2",
+    "url": "/static/media/itemTemp.8328a6c8.jpg"
+  },
+  {
+    "revision": "ddf8b5788285fdf15f8713f6dcf56422",
+    "url": "/static/media/model.ddf8b578.jpg"
+  },
+  {
+    "revision": "9bbd4983043b7ce3b8277235d280c148",
+    "url": "/static/media/new1.9bbd4983.jpg"
+  },
+  {
+    "revision": "560bf3391868baf5999cece4b864472e",
+    "url": "/static/media/new2.560bf339.jpg"
+  },
+  {
+    "revision": "bd1a829c34cab196b49e0c0f657c1c8c",
+    "url": "/static/media/new3.bd1a829c.jpg"
+  },
+  {
+    "revision": "0a95648df9a7cd33de3c5ff8fe93ac32",
+    "url": "/static/media/new4.0a95648d.jpg"
+  },
+  {
+    "revision": "dd960c1f29cb800324a0c53702bdb5c7",
+    "url": "/static/media/new5.dd960c1f.jpg"
+  },
+  {
+    "revision": "e0b1d34973aa03eca51ef796afc960ad",
+    "url": "/static/media/new6.e0b1d349.jpg"
+  },
+  {
+    "revision": "3ed90bf675ba14375debbf4feb4f3356",
+    "url": "/static/media/tamta.3ed90bf6.jpg"
+  },
+  {
+    "revision": "6c3c90ccf08567b1c7c0b4c060dd88af",
+    "url": "/static/media/temp1.6c3c90cc.jpg"
+  },
+  {
+    "revision": "a7f5f45b40d616a78c2f4c5c5aca7240",
+    "url": "/static/media/temp2.a7f5f45b.jpg"
+  },
+  {
+    "revision": "e6bc6cb521e4df6b9a9be7ebade7d25f",
+    "url": "/static/media/temp3.e6bc6cb5.jpg"
+  },
+  {
+    "revision": "52feb500f9e7dd61199490990302786d",
+    "url": "/static/media/temp4.52feb500.jpg"
+  }
+]);
