89aaa171ed3f3920bfb7416e8a9f0cc3963eadc1
ymwho <parametertree@gmail.com>
2020-10-22 00:30:33 +0900

    gnb:fade update

--- js/gnbConvert.js
+++ js/gnbConvert.js
@@ -4,8 +4,10 @@ function gnbFxiedHandle() {
   document.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       gnbFixed.style.backgroundColor = '#1F1F1F';
+      gnbFixed.style.transition = 'all 300ms ease-in-out';
     } else {
       gnbFixed.style.backgroundColor = 'transparent';
+      gnbFixed.style.transition = 'all 300ms ease-in-out';
     }
   });
 }
